9c56d675fa1ea2801ffd132295853ecc6300600a
Recleph M <lilrayzer1@gmail.com>
2021-10-05 05:19:49 -0700

    Create initial application with Required features

---
+++ Flix/MoviesViewController.swift
@@ -0,0 +1,58 @@
+//
+//  MoviesViewController.swift
+//  Flix
+//
+//  Created by Recleph Mere on 10/4/21.
+//
+
+import UIKit
+import AlamofireImage
+
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    var movies = [[String:Any]]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&language=en-US&page=1")!
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        session.configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
+        let task = session.dataTask(with: url) { (data, response, error) in
+           if let error = error {
+              print(error.localizedDescription)
+           } else if let data = data,
+              let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+            self.movies = dataDictionary["results"] as! [[String:Any]]
+            self.tableView.reloadData()
+            
+              // Handle dataDictionary
+           }
+        }
+        task.resume()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieCell
+        let movie = movies[indexPath.row]
+        let title = movie["title"]
+        let overview = movie["overview"]
+        let baseURL = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterURL = URL(string: baseURL + posterPath)!
+        cell.movieTitle.text = title as? String
+        cell.movieDescription.text = overview as? String
+        cell.posterView.af.setImage(withURL: posterURL)
+        return cell
+    }
+
+}
+
